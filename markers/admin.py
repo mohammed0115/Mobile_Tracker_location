7865bfa2dd17dd16fc8d5deab88fc670e991d9c8
mohammed0115 <mohemmedkamal0115498898@gmail.com>
2023-02-17 12:50:54 +0300

    data

--- markers/admin.py
+++ markers/admin.py
@@ -37,4 +37,9 @@ admin.site.register(map_models.Search, gis_admin.GeoModelAdmin)
 
 @admin.register(Marker)
 class MarkerAdmin(admin_gis.GeoModelAdmin):
-    list_display = ("name", "location")
+    list_display = ['ip', 'network', 'version', 'city', 'region', 'region_code', 
+                    'country', 'country_name', 'country_code', 'country_code_iso3', 
+                    'country_capital', 'country_tld', 'continent_code', 'in_eu',
+                      'postal', 'latitude', 'longitude', 'timezone', 'utc_offset',
+                        'country_calling_code',
+                     'currency', 'currency_name', 'languages', 'country_area', 'country_population', 'asn', 'org']
--- markers/models.py
+++ markers/models.py
@@ -2,14 +2,80 @@
 
 # Create your models here.
 from django.contrib.gis.db import models
+from account.models import User
 
+"""
+{
+    "ip": "102.121.219.69",
+    "network": "102.121.219.0/24",
+    "version": "IPv4",
+    "city": "Khartoum",
+    "region": "Khartoum",
+    "region_code": "KH",
+    "country": "SD",
+    "country_name": "Sudan",
+    "country_code": "SD",
+    "country_code_iso3": "SDN",
+    "country_capital": "Khartoum",
+    "country_tld": ".sd",
+    "continent_code": "AF",
+    "in_eu": false,
+    "postal": null,
+    "latitude": 15.582,
+    "longitude": 32.537,
+    "timezone": "Africa/Khartoum",
+    "utc_offset": "+0200",
+    "country_calling_code": "+249",
+    "currency": "SDG",
+    "currency_name": "Pound",
+    "languages": "ar-SD,en,fia",
+    "country_area": 1861484.0,
+    "country_population": 41801533,
+    "asn": "AS36972",
+    "org": "MTN"
+}
 
+"""
 class Marker(models.Model):
-    name = models.CharField(max_length=255)
+    ip = models.CharField(max_length=15)
+    network=models.CharField(max_length=15)
+    network=models.CharField(max_length=15)
+    city =models.CharField(max_length=70)
+    region =models.CharField(max_length=70)
+    region_code =models.CharField(max_length=7)
+    country =models.CharField(max_length=7)
+    country_name =models.CharField(max_length=70)
+    country_code =models.CharField(max_length=7)
+    country_code_iso3 =models.CharField(max_length=7)
+    country_capital =models.CharField(max_length=70)
+    country_tld =models.CharField(max_length=7)
+    continent_code  = models.CharField(max_length=4)
+    in_eu  = models.BooleanField()
+    postal  = models.CharField(max_length=100)
+    timezone  = models.CharField(max_length=70)
+    utc_offset  = models.CharField(max_length=10)
+    country_calling_code  = models.CharField(max_length=7)
+    currency  = models.CharField(max_length=10)
+    currency_name  = models.CharField(max_length=40)
+    languages  = models.CharField(max_length=100)
+    country_area  = models.CharField(max_length=40)
+    asn  = models.CharField(max_length=20)
+    org = models.CharField(max_length=20)
+    country_population  = models.CharField(max_length=40)
+    latitude  = models.DecimalField(max_digits=10, decimal_places=6)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
     location = models.PointField()
-
+    user = models.ForeignKey()
     def __str__(self):
-        return self.name
+        return self.ip
+    
+    # def save(self, *args, **kwargs):
+    #     self.location.y = self.Latitude 
+    #     self.mpoint.x   = self.Longitude    
+    #     super(Marker, self).save(*args, **kwargs)  
+
+
+
 from django.db import models
 
 
