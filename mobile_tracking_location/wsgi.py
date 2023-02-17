80ccc4f29bf6091cf0dd903c3f65a0213cc17825
mohammed0115 <mohemmedkamal0115498898@gmail.com>
2023-02-17 08:38:43 +0000

    update the server

--- mobile_tracking_location/wsgi.py
+++ mobile_tracking_location/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+sys.path.append('/var/www/Mobile_Tracker_location/mobile_tracking_location')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile_tracking_location.settings')
