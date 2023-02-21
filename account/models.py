925728f3fba1d2be3f6f4b8b9782af4666077937
mohammed0115 <mohemmedkamal0115498898@gmail.com>
2023-02-21 18:17:58 +0300

    change account

--- account/models.py
+++ account/models.py
@@ -127,13 +127,13 @@ class User(AbstractBaseUser,PermissionsMixin):
     def has_perm(perm, obj=None):
         # "Does the user have a specific permission?"
         # Simplest possible answer: Yes, always
-        return True
+        return False
 
-    # @staticmethod
-    # def has_module_perms(app_label):
-    #     # "Does the user have permissions to view the app `app_label`?"
-    #     # Simplest possible answer: Yes, always
-    #     return True
+    @staticmethod
+    def has_module_perms(app_label):
+        # "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return False
 
     def __str__(self):
         return "{}".format(self.email)
